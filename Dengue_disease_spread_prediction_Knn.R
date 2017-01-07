dd382a86181ea0a900fff8d6b0776bae4e103bbd
Akshayaa Vaidyanathan <akshayaavaidya@gmail.com>
2017-01-07 13:57:11 +0000

    Add files via upload
    Using Knn regression algorithm

---
+++ Dengue_disease_spread_prediction_Knn.R
@@ -0,0 +1,25 @@
+dat = read.csv("dengue_features_train.csv", header = TRUE)
+dat_labels = read.csv("dengue_labels_train.csv", header = TRUE)
+dat_test =read.csv("dengue_features_test.csv", header = TRUE)
+training_dat<- merge(dat,dat_labels,by=c("city","year","weekofyear"))
+training <- training_dat[,5:dim(training_dat)[2]]
+// training<- merge(dat,dat_labels,by=c("city","year","weekofyear"))[,-(1:4)]
+
+test<- dat_test[,-(1:4)]
+
+// handling missing values in the test data - replace mean for missing values
+for (i in 1:dim(test)[2]) {
+	means=colMeans(test[i],na.rm=TRUE)
+	test[i][is.na(test[i])]= means }
+
+deng_kknn <- kknn(total_cases~., training, test, distance = 1, na.action=na.pass, kernel = "triangular")
+fit <- fitted(deng_kknn)
+fit<-data.frame(fit)
+
+test1<-dat_test["city"]
+test2<-cbind(test1,dat_test["weekofyear"],dat_test["year"])
+
+test2<-cbind(test2,round(fit["fit"]))
+
+write.csv(test2, file = "F:\\Data driven competitions\\predict_dengue\\predictions.csv", fileEncoding = "UTF-16LE")
+
